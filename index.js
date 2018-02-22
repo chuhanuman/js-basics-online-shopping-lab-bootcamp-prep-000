31125d85b4519856044ae096ef49469c9affe34b
Hanuman Chu <chuhanuman@gmail.com>
2018-02-22 22:28:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
-    
+    console.log(`Your total cost is $${}, which will be charged to the card ${cardNumber}.`);
   } else {
-    
+    console.log("Sorry, we don't have a credit card on file for you.");
   }
 }
 viewCart();
