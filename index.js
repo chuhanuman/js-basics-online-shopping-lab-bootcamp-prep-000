85b4a552c794c2ea460a675df67d6fe2ff2668e6
Hanuman Chu <chuhanuman@gmail.com>
2018-02-22 21:31:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
   }
   for (let i=0;cart.length>i;i++){
     if (i === cart.length-1){
-      temp = temp + `and ${i} at $${i}.`;
+      temp = temp + `and ${cart[0][0]} at $${i}.`;
     } else {
       temp = temp + `${i} at $${i}, `;
     }
